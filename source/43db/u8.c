0159994656801b1be56ff4387d74a17c5ef5f0e8
Naim2000 <naimarnohero@gmail.com>
2024-08-06 10:53:33 -0500

    U8: more index-based code

--- source/43db/u8.c
+++ source/43db/u8.c
@@ -6,7 +6,7 @@
 
 #include "u8.h"
 
-#define ERROR(str, ...) fprintf(stderr, "%s: \n" str "\n", __PRETTY_FUNCTION__, ##__VA_ARGS__)
+#define ERROR(str, ...) fprintf(stderr, "%s:%i: \n" str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 int U8Init(void* ptr, size_t fsize, U8Context* ctx) {
     assert(ptr != NULL && ctx != NULL);
@@ -16,46 +16,47 @@ int U8Init(void* ptr, size_t fsize, U8Context* ctx) {
     U8Header* header = (U8Header*)ptr;
 
     if (header->magic != U8_MAGIC) {
+        ERROR("U8 header magic is invalid (%#08x != %#08x)", header->magic, U8_MAGIC);
         return -1;
     }
     
     U8Node* nodes = (U8Node*)(ptr + header->root_node_offset);
     U8Node* root_node = &nodes[0];
-    unsigned int node_count = root_node->size;
-
-    const char* str_table = (const char*)(&nodes[node_count]);
-    const char* str_table_end = ptr + header->root_node_offset + header->meta_size;
-    unsigned int str_table_size = (unsigned int)(str_table_end - str_table);
 
     if (root_node->type != 0x01) {
         ERROR("Root node is not a directory? What?");
         return -2;
     }
 
-    for (U8Node* node = &nodes[1]; node < nodes + node_count; node++) {
-        unsigned int node_index = (node - nodes);
+    unsigned int node_count = root_node->size;
+
+    const char* str_table = (const char*)(nodes + node_count);
+    unsigned int str_table_size = header->meta_size - (node_count * sizeof(U8Node));
+
+    for (unsigned int index = 1; index < node_count; index++) {
+        U8Node* node = &nodes[index];
 
         if (node->type != 0x00 && node->type != 0x01) {
-            ERROR("Node #%i has invalid node type (%#x)", node_index, node->type);
+            ERROR("Node #%u has invalid node type (%#x)", index, node->type);
             return -3;
         }
 
-        const char* node_name = str_table + node->name_offset;
-        if (node_name >= str_table_end) {
-            ERROR("Name for node #%i is out of range (name_offset=%#x)", node_index, node->name_offset);
+        if (node->name_offset >= str_table_size) {
+            ERROR("Name offset for node #%u is out of range (%#x >= %#x)", index, node->name_offset, str_table_size);
             return -4;
         }
 
-        // OSReport("Node #%u, name: +%#x '%s', type=%#x, ofs=%u", node_index, node->name_offset, node_name, node->type, node->offset);
+        const char* node_name = str_table + node->name_offset;
+        // OSReport("Node #%u, name: +%#x '%s', type=%#x, ofs=%u", index, node->name_offset, node_name, node->type, node->offset);
 
         if (node->type == 0x00) {
-            if (node->offset < header->data_offset || (fsize && node->offset > fsize)) {
-                ERROR("Data offset for file node #%i (%s) is out of bounds (=%#x)", node_index, node_name, node->offset);
+            if (node->offset < header->data_offset) {
+                ERROR("Data offset for file node #%u (%s) is out of bounds (%#x < %#x)", index, node_name, node->offset, header->data_offset); // Only use for data_offset lmao
                 return -5;
             }       
         } else {
             if (node->offset > node_count) {
-                ERROR("End marker for directory node #%i (%s) is out of bounds (%#x>%#x)", node_index, node_name, node->offset, node_count);
+                ERROR("End marker for directory node #%u (%s) is out of bounds (%#x > %#x)", index, node_name, node->offset, node_count);
                 return -5;
             }
         }
@@ -69,33 +70,33 @@ int U8Init(void* ptr, size_t fsize, U8Context* ctx) {
     ctx->str_table_size = str_table_size;
 
     ctx->ptr = ptr;
-    ctx->fsize = fsize;
     return 0;
 }
 
-static U8Node* U8FindChild(U8Context* ctx, U8Node* parent, const char* name) {
-    if (parent->type != 0x01) {
+static unsigned int U8FindChild(U8Context* ctx, unsigned int parent, const char* name) {
+    if (ctx->nodes[parent].type != 0x01) {
         ERROR("Tried to find child in a non-directory node");
-        return NULL;
+        return 0;
     }
 
-    U8Node* dir_ptr = parent + 1;
-    U8Node* dir_end = &ctx->nodes[parent->size];
+    unsigned int dir_cur = parent + 1;
+    unsigned int dir_end = ctx->nodes[parent].size;
 
-    while (dir_ptr < dir_end) {
-        const char* node_name = ctx->str_table + dir_ptr->name_offset;
+    while (dir_cur < dir_end) {
+        U8Node* node = &ctx->nodes[dir_cur];
+        const char* node_name = ctx->str_table + node->name_offset;
 
         if (!strcmp(name, node_name))
-            return dir_ptr;
+            return dir_cur;
 
         // Next child node
-        if (dir_ptr->type == 0x01)
-            dir_ptr = &ctx->nodes[dir_ptr->size];
+        if (node->type == 0x01)
+            dir_cur = node->size;
         else
-            dir_ptr++;
+            dir_cur++;
     }
 
-    return NULL;
+    return 0;
 }
 
 int U8OpenFile(U8Context* ctx, const char* filepath, U8File* out) {
@@ -107,7 +108,7 @@ int U8OpenFile(U8Context* ctx, const char* filepath, U8File* out) {
 
     char* saveptr;
     char* elem = strtok_r(_filepath, "/", &saveptr);
-    U8Node* node = ctx->root_node;
+    unsigned int index = 0;
 
     if (!elem) {
         ERROR("strtok() came back with nothing on the first run?\n" "'%s' <-- Does this path have a slash?", filepath);
@@ -116,23 +117,24 @@ int U8OpenFile(U8Context* ctx, const char* filepath, U8File* out) {
     }
 
     while (elem) {
-        node = U8FindChild(ctx, node, elem);
-        if (!node)
+        index = U8FindChild(ctx, index, elem);
+        if (!index)
             break;
 
         elem = strtok_r(NULL, "/", &saveptr);
     }
 
     free(_filepath);
-    if (!node)
+    if (!index)
         return -2;
 
+    U8Node* node = &ctx->nodes[index];
     if (node->type != 0x00)
         return -3;
 
     if (out) {
         out->ctx = ctx;
-        out->node_index = node - ctx->nodes;
+        out->node_index = index;
         out->data_ptr = ctx->ptr + node->offset;
         out->fsize = node->size;
     }
